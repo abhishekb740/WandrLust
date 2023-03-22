ca24290bfcb0ee9a0d96ee11538385fb27a03308
abhishekb740 <abhishekb740@gmail.com>
2023-03-22 18:40:27 +0530

    Changes in index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about", {
-    name: user.name.substring(0, user.name.indexOf(" ")),
+    name: user.name.substring(0, user.name.indexOf(" ")) ? user.name.substring(0, user.name.indexOf(" ")) : user.name,
     isLoggedIn: isLoggedIn,
     email: user.email,
     username: user.username,
@@ -48,7 +48,7 @@ app.get("/test", (req, res) => {
 
 app.get("/faq", (req, res) => {
   res.render("faq", {
-    name: user.name.substring(0, user.name.indexOf(" ")),
+    name: user.name.substring(0, user.name.indexOf(" ")) ? user.name.substring(0, user.name.indexOf(" ")) : user.name,
     isLoggedIn: isLoggedIn,
     email: user.email,
     username: user.username,
@@ -90,7 +90,7 @@ app.post("/sendMail", (req, res) => {
 app.get("/budget", (req, res) => {
   if (isLoggedIn) {
     res.render("budget", {
-      name: user.name.substring(0, user.name.indexOf(" ")),
+      name: user.name.substring(0, user.name.indexOf(" ")) ? user.name.substring(0, user.name.indexOf(" ")) : user.name,
       isLoggedIn: isLoggedIn,
       email: user.email,
       username: user.username,
@@ -102,7 +102,7 @@ app.get("/budget", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.render("contact", {
-    name: user.name.substring(0, user.name.indexOf(" ")),
+    name: user.name.substring(0, user.name.indexOf(" ")) ? user.name.substring(0, user.name.indexOf(" ")) : user.name,
     isLoggedIn: isLoggedIn,
     email: user.email,
     username: user.username,
@@ -126,7 +126,7 @@ app.get("/profile", (req, res) => {
     });
   } else {
     res.render("home", {
-      name: user.name.substring(0, user.name.indexOf(" ")),
+      name: user.name.substring(0, user.name.indexOf(" ")) ? user.name.substring(0, user.name.indexOf(" ")) : user.name,
       isLoggedIn: isLoggedIn,
       email: user.email,
       username: user.username,
@@ -137,7 +137,7 @@ app.get("/profile", (req, res) => {
 app.get("/post",(req,res)=>{
   if(isLoggedIn){
     res.render("post", {
-      name: user.name.substring(0, user.name.indexOf(" ")),
+      name: user.name.substring(0, user.name.indexOf(" ")) ? user.name.substring(0, user.name.indexOf(" ")) : user.name,
       isLoggedIn: isLoggedIn,
       email: user.email,
       username: user.username,
@@ -151,7 +151,7 @@ app.get("/post",(req,res)=>{
 app.get("/feed",(req,res)=>{
   if(isLoggedIn){
     res.render("feed",{
-      name: user.name.substring(0, user.name.indexOf(" ")),
+      name: user.name.substring(0, user.name.indexOf(" ")) ? user.name.substring(0, user.name.indexOf(" ")) : user.name,
       isLoggedIn: isLoggedIn,
       email: user.email,
       username: user.username,
@@ -165,7 +165,7 @@ app.get("/feed",(req,res)=>{
 app.get("/locations", (req, res) => {
   if (isLoggedIn) {
     res.render("locations", {
-      name: user.name.substring(0, user.name.indexOf(" ")),
+      name: user.name.substring(0, user.name.indexOf(" ")) ? user.name.substring(0, user.name.indexOf(" ")) : user.name,
       isLoggedIn: isLoggedIn,
       email: user.email,
       username: user.username,
