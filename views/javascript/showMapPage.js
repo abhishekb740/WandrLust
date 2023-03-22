c5f6ceceb23d6aef395b2e3b1a0953d1055c27da
Vaibhav Pandey <vaibhav.pandey0806@gmail.com>
2023-03-22 18:16:39 +0530

    Delete map js file

--- views/javascript/showMapPage.js
+++
@@ -1,7 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoidmFpYmhhdjA4MDYiLCJhIjoiY2xmaTFxMGc2MDd2bzN5cDJ1d2ExeDBtaSJ9.ksRFszOL1mPE2B0oxLvAJA';
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [79.9802, 13.5269], // starting position [lng, lat]
-    zoom: 10, // starting zoom
-});
